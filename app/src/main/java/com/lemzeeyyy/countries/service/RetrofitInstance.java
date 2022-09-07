eff0be76bfc859d3d1675e93d482d7cf0e19a8f4
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-09-07 01:14:40 +0100

    retrofit instance

---
+++ app/src/main/java/com/lemzeeyyy/countries/service/RetrofitInstance.java
@@ -0,0 +1,23 @@
+package com.lemzeeyyy.countries.service;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+    private static Retrofit retrofit = null;
+    private static String BASE_URL = "https://api.printful.com/";
+
+    //Singleton Pattern for retrofit instance
+    public static GetCountryDataService getService(){
+        if(retrofit == null){
+            retrofit = new Retrofit
+                    .Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+
+        return retrofit.create(GetCountryDataService.class);
+    }
+}
